767a4e0b4a5a0f5bcec135ebf8bd77ea381dc03a
Trinidad Boragini <mtboragini@gmail.com>
2021-09-21 11:24:49 -0300

    Viendo ciclo de vida del componente

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import RenderizadoCondicional from './components/RenderizadoCondicional';
 import RenderizadoElementos from './components/RenderizadoElementos';
 import { EventosES6, EventosES7, MasSobreEventos } from './components/Eventos';
 import ComunicacionComponentes from './components/ComunicacionComponentes';
+import CicloVida from './components/CicloVida';
 import Estado from './components/Estado';
 import './App.css';
 
@@ -54,6 +55,8 @@ function App() {
           <MasSobreEventos />
           <hr />
           <ComunicacionComponentes />
+          <hr />
+          <CicloVida />
 
         </section>
       </header>
